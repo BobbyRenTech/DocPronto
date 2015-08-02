97890e4a8900bc99bd75392c69a792c5655ba967
Roeut Chak <roeut.chak@gmail.com>
2015-08-02 04:37:44 -0400

    added map, compiled, but map is blank

---
+++ DocPronto/MapViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MapViewController.swift
+//  DocPronto
+//
+//  Created by Roeut on 8/2/15.
+//  Copyright (c) 2015 Bobby Ren. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+   // @IBOutlet var mapView: GMSMapView!
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+  //      var camera = GMSCameraPosition.cameraWithLatitude(39.952432,longitude: –75.164403, zoom: 15)
+        
+   //     self.mapView.myLocationEnabled = true
+        
+   //     self.mapView.camera = camera
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
